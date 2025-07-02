ce2ecf9a82b8777e3113b3373565e518aa76a18f
Vishal Govind <vishal.govind2098@gmail.com>
2025-07-03 02:30:10 +0530

    hitting backend from client

--- server/main.go
+++ server/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
+	router.Use(cors.Default())
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
